fe3237932fd3c6bfae4b38f69d424fc7647b3629
Luukas Mäkilä <luukas.makila@kolumbus.fi>
2023-01-25 19:30:35 +0200

    initialize weights and biases

---
+++ neural_network/neural_network.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class NeuralNetwork:
+
+    def __init__(self, layer_sizes):
+        self.number_of_layers = len(layer_sizes)
+        self.layer_sizes = layer_sizes
+        self.weights, self.biases = self.init_weights_and_biases()
+    
+    def init_weights_and_biases(self):
+        """
+        Initialize weights and biases, ignore input layer
+        """
+        biases = [np.random.randn(i, 1) for i in self.layer_sizes[1:]]
+
+        weights = []
+        for x, y in zip(self.layer_sizes[:-1], self.layer_sizes[1:]):
+            weights.append(np.random.randn(x,y)/np.sqrt(x))
+
+        return weights, biases
+
+nn = NeuralNetwork([784,16,10])
+print(len(nn.biases))
+print(len(nn.weights))
